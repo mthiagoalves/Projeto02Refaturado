1e729f460c87cc18d3a5878ec80b72e7284a2055
mthiagoalves <m.thiago.alves@live.com>
2022-03-21 22:42:21 -0300

    andamento

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express ();
 app.set("view engine", "ejs");
 const path = require ("path");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
 const port = 8081;
 
 const pokedex = [
@@ -16,28 +18,37 @@ const pokedex = [
     },
     {   
         id: 002,
-        nome: "Charmander ",
+        nome: "Charmander",
         imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
         descricao: "From the time it is born, a flame burns at the tip of its tail. Its life would end if the flame were to go out.",
         tipo: "Fire"
     },
     {   
         id: 003,
-        nome: "Squirtle ",
+        nome: "Squirtle",
         imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
         descricao: "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
         tipo: "Water"
+    },
+    {   
+        id: 004,
+        nome: "Caterpie",
+        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png",
+        descricao: "For protection, it releases a horrible stench from the antenna on its head to drive away enemies.",
+        tipo: "Bug"
     }
-]
+];
 
 app.get("/", (req, res) => {
     res.render("index", {pokedex});
 });
 
 app.post("/add", (req, res) => {
-    res.send(`Olá`)
+    const pokemon = req.body;
+    pokedex.push(pokemon);
+    console.log(pokedex)
+    res.redirect("/");
 });
-
 
 app.listen(port, () =>{
     console.log(`Servidor rodando na URL http://localhost:${port}`)
